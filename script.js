ef398a7b0e77cfe3435907a8c01c26ef999b67c0
khusnimbrkh <khusnimbrkh@gmail.com>
2023-12-13 18:13:57 +0700

    menambakan icon

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const whatsappButton = document.getElementById("whatsapp-button");
+  
+    whatsappButton.addEventListener("click", function() {
+      // Ganti URL di bawah ini dengan URL Anda sendiri untuk membuka obrolan WhatsApp
+      window.open("https://wa.me/088290576179", "_blank");
+    });
+  });
+  
